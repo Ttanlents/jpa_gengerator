9c9e3112e74fbb97f327af29e962065a41dbba8d
Ttanlents <825362171@qq.com>
2020-11-14 17:35:51 +0800

    Initial commit

---
+++ src/main/java/com/yjf/JpaGengeratorApplication.java
@@ -0,0 +1,13 @@
+package com.yjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaGengeratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaGengeratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjf/JpaGengeratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaGengeratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
